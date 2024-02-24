3bf20cbe420ccf4562c9599bc912b1524fc1f77b
ramilsaavedra <ramilsmcl@gmail.com>
2024-02-25 06:41:06 +0800

    feat: add background img on each routes / pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image"
 
 export default function Home() {
-  return <main className="">HomePage</main>
+  return (
+    <main className="">
+      HomePage
+      <div className="absolute inset-0 z-[-1]">
+        <Image src="/home.jpg" alt="car factory" fill={true} />
+      </div>
+    </main>
+  )
 }
